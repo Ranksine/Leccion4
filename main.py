8d4524de33342206022b2ec8c85f823923934ea7
_Arom <117594980+Ranksine@users.noreply.github.com>
2024-03-16 22:55:55 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+"""
+condicion = True
+
+while condicion:
+    print('Ejecutando ciclo while')
+else:
+    print('Fin del ciclo while')
+"""
+"""
+contador = 0
+while contador < 3:
+    print(contador)
+    contador += 1  
+else:
+    print('Fin ciclo while')
+"""
+
+# Ejercicio: Imprimir los números enteros del 0 al 5 utilizando un ciclo while
+# maximo = 5
+# contador = 0
+# while contador <= maximo:
+#     print(contador)
+#     contador += 1
+
+# Ejercicio: Imprimir los números enteros del 5 al 1 utilizando un ciclo while
+minimo = 1
+contador = 5
+while contador >= minimo:
+    print(contador)
+    contador -= 1
+
